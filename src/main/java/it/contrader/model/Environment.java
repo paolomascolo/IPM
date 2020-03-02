c8a5f8c35ca7722a6534aec45ce7ff578ea3200d
michelebonanno1993 <michele.bonanno993gmail.com>
2020-03-02 11:11:19 +0100

    Modified and fixed Environment-Packet relationship in DB

--- src/main/java/it/contrader/model/Environment.java
+++ src/main/java/it/contrader/model/Environment.java
@@ -6,7 +6,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
+import javax.persistence.ManyToOne;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -27,7 +27,7 @@ public class Environment {
 	
 	private String description;
 
-	@OneToOne
+	@ManyToOne
 	private Packet environmentpacket; 
 	
 }
